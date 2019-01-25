1bb8bc75150582b9ee0f1c1a0e3fbdebcc6c17ee
LazyLacey <krahobory5273@gmail.com>
2019-01-25 07:29:58 -0800

    homework 3 ready

---
+++ main.js
@@ -0,0 +1,21 @@
+﻿function printWindowValues(){
+  console.log(window)
+}
+printWindowValues()
+
+function expTen(a){
+  var b=a
+  for(var i = 1; i < 10; i++){
+    a = b*a
+
+  }
+  console.log(a)
+}
+expTen(2)
+
+
+function getFuncRes(method,value){
+   method(value)
+}
+
+getFuncRes(console.log,"hello world");
